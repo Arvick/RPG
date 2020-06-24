2c0b8c8efc7b466873210a08c37cebf3780a829e
Arvick <macalaladjustin@gmail.com>
2020-06-23 17:27:18 -0700

    Update RPG.py

--- RPG.py
+++ RPG.py
@@ -1,12 +1,26 @@
-class Player(self,name,atk,defense,spd,luck,wallet,*inventory,exp,level):
-    self.name=name
-    self.atk=atk
-    self.defense=defense
-    self.spd=spd
-    self.luck=luck
-    self.wallet=wallet
-    self.inventory=inventory
-    self.exp=exp
-    self.level=level
+class Player:
+    def __init__(self,name,atk,defense,spd,luck,wallet,*inventory,exp,level):
+        self.name=name
+        self.atk=atk
+        self.defense=defense
+        self.spd=spd
+        self.luck=luck
+        self.wallet=wallet
+        self.inventory=inventory
+        self.exp=exp
+        self.level=level
+    
+    def __repr__(self):
+        return(f'''
+        {self.name}
+        Attack:{self.atk}
+        Defense:{self.defense}
+        Speed:{self.spd}
+        Luck:{self.luck}
+        Wallet:{self.wallet}
+        Inventory:{self.inventory}
+        EXP:{self.exp}
+        Level:{self.level}
+        ''')
 
 #class Item(self,name)
