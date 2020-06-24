fb95f6981d62a86c2d5a683598136297c63496e6
Arvick <macalaladjustin@gmail.com>
2020-06-23 17:08:34 -0700

    Start
    Began working on class for Player. Maybe classes for other things, like enemies, rooms, or items.

--- RPG.py
+++ RPG.py
@@ -1,1 +1,12 @@
-#test
+class Player(self,name,atk,defense,spd,luck,wallet,*inventory,exp,level):
+    self.name=name
+    self.atk=atk
+    self.defense=defense
+    self.spd=spd
+    self.luck=luck
+    self.wallet=wallet
+    self.inventory=inventory
+    self.exp=exp
+    self.level=level
+
+#class Item(self,name)
